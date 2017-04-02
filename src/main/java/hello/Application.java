d0866c3b312d4bb97625b583bccef1993b796993
Sowmya Parameshwara <cjsowm@gmail.com>
2017-04-02 00:41:51 -0500

    Web security application

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Sowmya on 3/17/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
